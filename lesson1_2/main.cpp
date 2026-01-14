1f9540aa2bebf3154c0d56194f6b4b045eca73d6
ESJIAN <3340580252@qq.com>
2026-01-14 12:23:34 +0800

    update(gitignore):更新了忽略条目

---
+++ lesson1_2/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "add.hpp"
+
+int main() {
+
+    int sum = add_(3, 5);
+    std::cout << "Sum of 3 and 5 is: " << sum << std::endl;
+    std::cout << "Hello, World!" << std::endl;
+    
+    return 0;
+}
